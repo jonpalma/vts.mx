b74c0dc840d36ff731efefb3f8148cd0a96db2fb
chrisJaquez <chris.jaquez@mixen.mx>
2015-07-28 11:04:43 -0600

    registro.html

--- web/js/app.js
+++ web/js/app.js
@@ -55,3 +55,60 @@ $(function() {
 	});
 
 });
+
+$(function() {
+
+	// Get the form.
+	var form = $('#registro-form');
+
+	// Get the messages div.
+	var formMessages = $('#form-messages-registro');
+
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+        $("#submit").text("Enviando...");
+
+		// Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('alert alert-danger');
+			$(formMessages).addClass('alert alert-success');
+
+			// Set the message text.
+			$(formMessages).text(response);
+
+			// Clear the form.
+			$('#nombre').val('');
+            $('#tel').val('');
+			$('#email').val('');
+			$('#empresa').val('');
+            $("#submit").text("Enviar");
+		})
+		.fail(function(data) {
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages).removeClass('alert alert-success');
+			$(formMessages).addClass('alert alert-danger');
+
+			// Set the message text.
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+                $("#submit").text("Enviar");
+			} else {
+				$(formMessages).text('Oops! Ocurrió un error no se pudo enviar la forma.');
+                $("#submit").text("Enviar");
+			}
+		});
+
+	});
+
+});
